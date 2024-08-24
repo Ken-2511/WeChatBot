18679e1c63f4712ca0f304c76eacae56e3720501
Ken-2511 <IWMAIN@outlook.com>
2024-08-24 13:23:19 +0800

    big change: train the neuro network to classify the type of message

--- utils/message_classifier.py
+++ utils/message_classifier.py
@@ -1,80 +1,164 @@
 # 训练一个AI来检测一张消息截图的类型（文字，图片，表情包……）
+import os
+import cv2
+import json
 import torch
+import numpy as np
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from torchvision.models import mobilenet_v2
+from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
 
+__all__ = ["MessageClassifier"]
+
+file_dir = os.path.dirname(__file__)
 
 # 所有消息类型
-all_types = ["text", "system", "sticker", "image", "file", "mini-program", "voice", "voice/video call", "other"]
+all_types = ["text", "system", "sticker", "image", "tickle", "file", "voice", "voice/video call", "mini-program", "other"]
+_weights = {
+    "text": 1,
+    "system": 2,
+    "sticker": 3,
+    "image": 4,
+    "tickle": 1,
+    "file": 6,
+    "voice": 6,
+    "voice/video call": 6,
+    "mini-program": 6,
+    "other": 3
+}
+_ = sum(_weights.values())
+type_weights = torch.tensor([_weights[t] / _ for t in all_types], dtype=torch.float32)
 
 
-# class MessageClassifier(nn.Module):
-#     def __init__(self):
-#         super(MessageClassifier, self).__init__()
-#         self.model = mobilenet_v2(pretrained=True)
-#         self.model.classifier = nn.Sequential(
-#             nn.Dropout(p=0.2),
-#             nn.Linear(in_features=1280, out_features=128),
-#             nn.ReLU(),
-#             nn.Dropout(p=0.2),
-#             nn.Linear(in_features=128, out_features=len(all_types))
-#         )
-#
-#     def forward(self, x):
-#         return self.model(x)
-#
-#
-# class MyDataset(Dataset):
-#     def __init__(self, data, target):
-#         self.data = data
-#         self.target = target
-#         self.transform = transforms.Compose([
-#             transforms.ToPILImage(),
-#             transforms.Resize((224, 224)),
-#             transforms.ToTensor()
-#         ])
-#
-#     def __len__(self):
-#         return len(self.data)
-#
-#     def __getitem__(self, idx):
-#         return self.transform(self.data[idx]), self.target[idx]
-#
-#
-# def train(model, train_loader, val_loader, epochs=10, lr=0.001):
-#     criterion = nn.CrossEntropyLoss()
-#     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-#
-#     for epoch in range(epochs):
-#         model.train()
-#         for x, y in train_loader:
-#             optimizer.zero_grad()
-#             output = model(x)
-#             loss = criterion(output, y)
-#             loss.backward()
-#             optimizer.step()
-#
-#         model.eval()
-#         correct = 0
-#         total = 0
-#         with torch.no_grad():
-#             for x, y in val_loader:
-#                 output = model(x)
-#                 _, predicted = torch.max(output, 1)
-#                 total += y.size(0)
-#                 correct += (predicted == y).sum().item()
-#
-#         print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}, Accuracy: {correct / total}")
+class MessageClassifier(nn.Module):
+    def __init__(self, pretrained=False):
+        super(MessageClassifier, self).__init__()
+        self.model = mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
+        self.model.features.requires_grad_(False)  # freeze the feature extractor
+        self.model.classifier = nn.Sequential(
+            # nn.Dropout(p=0.2),
+            nn.Linear(in_features=1280, out_features=128),
+            nn.ReLU(),
+            # nn.Dropout(p=0.2),
+            nn.Linear(in_features=128, out_features=len(all_types))
+        )
+        if pretrained:
+            path = os.path.join(file_dir, "train_data", "checkpoints", "epoch50.pt")
+            self.load_state_dict(torch.load(path, weights_only=True))
+
+    def forward(self, x):
+        return self.model(x)
+
+    def predict_numpy(self, x):
+        # 作为对外的接口，接受numpy数组，返回预测结果
+        # 裁剪和缩放图片（一般来讲图片宽度741，将图片缩放到224x224）
+        h, w, _ = x.shape
+        new_h, new_w = 224 * h // w, 224
+        x = cv2.resize(x, (new_w, new_h))
+        new_x = np.ones((224, 224, 3), dtype=np.uint8) * 237
+        if new_h > 224:
+            new_x = x[:224, :]
+        else:
+            new_x[:new_h, :] = x[:, :]
+        x = new_x.astype(np.float32)
+        x = torch.from_numpy(x).permute(2, 0, 1).unsqueeze(0)
+        output = self(x)
+        _, predicted = torch.max(output, 1)
+        return all_types[predicted.item()]
 
 
-import os
-import json
-import tkinter as tk
-from tkinter import messagebox
-import cv2
-from PIL import Image, ImageTk
+class MyDataset(Dataset):
+    def __init__(self, file_path):
+        self.json_file = file_path
+        with open(self.json_file, "r") as file:
+            self.json_file = json.load(file)
+        self.data = []
+        self.target = []
+        for key, value in self.json_file.items():
+            self.data.append(key)
+            self.target.append(value)
+        self.transform = transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # read image
+        img = cv2.imread(f"train_data/msg_images/{self.data[idx]}", cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # pad the image, always maintain the width and pad the height,
+        # paste the image on the top of the padded image
+        h, w, _ = img.shape
+        new_image = np.ones((w, w, 3), dtype=np.uint8) * 237
+        if h > w:
+            new_image[:, :] = img[:w, :]
+        else:
+            new_image[:h, :] = img[:, :]
+        img = cv2.resize(img, (224, 224))
+        img = self.transform(img)
+        # process target
+        target = all_types.index(self.target[idx])
+        return img, target
+
+
+def train(model, train_loader, val_loader, epochs=10, lr=0.001, device="cuda"):
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss(weight=type_weights.to(device))
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(epochs):
+        model.train()
+        for x, y in train_loader:
+            x, y = x.to(device), y.to(device)
+            optimizer.zero_grad()
+            output = model(x)
+            loss = criterion(output, y)
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        total = 0
+        correct = 0
+        total_loss = 0
+        with torch.no_grad():
+            for x, y in val_loader:
+                x, y = x.to(device), y.to(device)
+                output = model(x)
+                loss = criterion(output, y)
+                total_loss += loss.item()
+                _, predicted = torch.max(output, 1)
+                total += y.size(0)
+                correct += (predicted == y).sum().item()
+        total_loss /= total
+
+        # update the lr
+        lr *= 0.95
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        # print the result
+        print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss.item():.4f}, Accuracy: {correct / total:.4f}, "
+              f"Val Loss: {total_loss:.4f}")
+        # save model
+        torch.save(model.state_dict(), f"train_data/checkpoints/epoch{epoch + 1}.pt")
+
+
+def show_results(model, val_loader):
+    # 随机抽取图片，用cv2显示图片，然后显示预测结果和置信度
+    while True:
+        idx = np.random.randint(len(val_loader.dataset))
+        x, y = val_loader.dataset[idx]
+        x = x.unsqueeze(0)
+        output = model(x)
+        _, predicted = torch.max(output, 1)
+        confidence = torch.softmax(output, 1).max().item()
+        img = cv2.imread(f"train_data/msg_images/{val_loader.dataset.data[idx]}", cv2.IMREAD_COLOR)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imshow("Image", img)
+        print(f"Predicted: {all_types[predicted.item()]}, Confidence: {confidence}")
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
+
 
 def rename_files():
     # 为了防止复制进来的文件和已有的文件重名，我们需要重命名文件
@@ -92,6 +176,9 @@ def rename_files():
 
 
 def label_data():
+    import tkinter as tk
+    from tkinter import messagebox
+    from PIL import Image, ImageTk
     # 指定json文件和图片文件夹
     json_file = "train_data/labels.json"
     img_folder = "train_data/msg_images"
@@ -209,23 +296,35 @@ def label_data():
 def print_label_stats():
     # 读取JSON文件
     json_file = "train_data/labels.json"
-
     with open(json_file, 'r') as f:
         data = json.load(f)
-
     # 初始化一个字典来统计每种类型的数量
     type_count = {msg_type: 0 for msg_type in all_types}
-
     # 统计每种类型的样本数量
+    total = 0
     for key, value in data.items():
         if value in type_count:
             type_count[value] += 1
-
+            total += 1
     # 打印结果
     for msg_type in all_types:
-        print(f"{msg_type}: {type_count[msg_type]} 个样本")
+        print(f"{msg_type}: {type_count[msg_type]} 个样本，占比 {type_count[msg_type] / total:.2%}")
 
 
 if __name__ == '__main__':
+    # label_data()
+    # split_train_val()
     # print_label_stats()
-    label_data()
+    # model = MessageClassifier()
+    # train_dataset = MyDataset("train_data/train_labels.json")
+    # train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    # val_dataset = MyDataset("train_data/val_labels.json")
+    # val_loader = DataLoader(val_dataset, batch_size=16)
+    # train(model, train_loader, val_loader, epochs=50, device="cuda")
+
+    model = MessageClassifier()
+    model.load_state_dict(torch.load("train_data/checkpoints/epoch50.pt", weights_only=True))
+    model.eval()
+    val_dataset = MyDataset("train_data/val_labels.json")
+    val_loader = DataLoader(val_dataset, batch_size=1)
+    show_results(model, val_loader)
